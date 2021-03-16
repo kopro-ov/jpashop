98ca3ac1a7d1c563cfe9e4c672297a59a5302b0c
acebogo <koprodev@gmail.com>
2021-03-16 23:35:04 +0900

    MemberRepository.java 경로 변경

--- src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
@@ -17,20 +17,18 @@ public class MemberRepository {
     public void save(Member member) {
         em.persist(member);
     }
-
     public Member findOne(Long id) {
         return em.find(Member.class, id);
     }
-
     public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
 
     public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+        return em.createQuery("select m from Member m where m.name = :name",
+                Member.class)
                 .setParameter("name", name)
                 .getResultList();
     }
-
 }
