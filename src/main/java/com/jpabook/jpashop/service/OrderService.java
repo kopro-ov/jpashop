22099359630628219211320ac0c7abf2a8458d97
acebogo <koprodev@gmail.com>
2021-03-17 00:52:39 +0900

    주문

---
+++ src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,52 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Delivery;
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.domain.item.Item;
+import com.jpabook.jpashop.repository.ItemRepository;
+import com.jpabook.jpashop.repository.MemberRepository;
+import com.jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문상품 생성
+        OrderItem orderItems = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItems);
+        
+        //주문 저장
+        orderRepository.save(order); //CascadeType.ALL했기 때문에 Order에서 한번만해도 되는것
+        //CascadeType.ALL은 신중히 써야함!
+
+        return order.getId();
+
+    }
+
+}
