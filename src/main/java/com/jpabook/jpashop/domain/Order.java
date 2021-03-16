03d9bf7bf7b5bbdd2c3e63703e8b51d79a82c46d
acebogo <koprodev@gmail.com>
2021-03-17 00:39:34 +0900

    주문 엔티티, 주문상품 엔티티 개발

--- src/main/java/com/jpabook/jpashop/domain/Order.java
+++ src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,7 @@ package com.jpabook.jpashop.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -50,6 +51,41 @@ public class Order {
     public void setDelivery(Delivery deliver) {
         this.deliver = deliver;
         deliver.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery deliver, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDeliver(deliver);
+        for(OrderItem orderItem: orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if(deliver.getStatus() == DeliverStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem: orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
     }
 
 }
