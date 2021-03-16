7375036d09332d2a2fc8c393c73cb629bc68e8ee
acebogo <koprodev@gmail.com>
2021-03-16 23:49:49 +0900

    중복_회원_예외 테스트 케이스 추가

--- src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,7 +26,7 @@ public class MemberServiceTest {
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
-        member.setName("kim2");
+        member.setName("kim");
 
         //when
         Long saveId = memberService.join(member);
@@ -34,15 +35,21 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.findOne(saveId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
 
+        Member member2 = new Member();
+        member2.setName("kim");
 
         //when
-
+        memberService.join(member1);
+        memberService.join(member2);
 
         //then
+        fail("예외가 발생해야 한다.");
 
     }
 
